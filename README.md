2a2303a7223ad49ab5b77d01bb4403c7869e2290
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2020-11-15 21:12:35 +0700

    fix sthg

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # CHESS
 Paper: https://arxiv.org/pdf/1711.09667v1.pdf
+## Run
+```
+$ python play.py
+```
--- app/main.py
+++ app/main.py
@@ -17,10 +17,10 @@ from utils import download_weights, featurize, compare, gen_compare_array
 device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
 
 ae_weight = download_weights('https://drive.google.com/uc?export=download&id=1ZsRsjDF8T3e44JwpIkS5drWjtfqocDrv')
-Siamese_weight = download_weights('https://drive.google.com/uc?export=download&id=1-MaSz_KX2xkYQJzL8f5-aA5kyamz_pjr')
+siamese_weight = download_weights('https://drive.google.com/uc?export=download&id=1-MaSz_KX2xkYQJzL8f5-aA5kyamz_pjr')
 
 ae_state = torch.load(ae_weight, map_location=lambda storage, loc: storage.cuda(0))
-siamese_state = torch.load(Siamese_weight, map_location=lambda storage, loc: storage.cuda(0))
+siamese_state = torch.load(siamese_weight, map_location=lambda storage, loc: storage.cuda(0))
 
 featurizer = AE().to(device)
 comparator = Siamese().to(device)
