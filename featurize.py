b4f632e5f9b7aa44c3091bf7f83229ba4b59ef07
tranhuuhuy297 <tranhuuhuy297@gmail.com>
2020-11-19 19:41:03 +0700

    fix sthg

--- featurize.py
+++ featurize.py
@@ -1,8 +1,13 @@
+import os
 import torch
 import numpy as np
 from utils import download_weights
 from model.auto_encoder import AE
 
+games = []
+
+for file in os.listdir('./data/bitboard'):
+    games.append(file)
 
 weight = download_weights('https://drive.google.com/uc?export=download&id=1ZsRsjDF8T3e44JwpIkS5drWjtfqocDrv')
 
@@ -12,16 +17,19 @@ state = torch.load(weight, map_location=lambda storage, loc: storage.cuda(0))
 
 model.load_state_dict(state)
 
-games = np.load('./data/bitboards.npy')
-
-# len: 554116 for 4000 epoch parse data
-batched_games = np.split(games, 19)
-
 def featurize(game):
     _, enc = model(torch.from_numpy(game).type(torch.FloatTensor))
     return enc.detach().numpy()
 
-feat_games = [featurize(batch) for batch in batched_games]
-featurized = np.vstack(feat_games)
+for game in list(games):
+    temp = np.load('data/bitboard/' + game)
 
-np.save('./data/features.npy', featurized)
+    # len: 554116 for 4000 epoch parse data
+    batched_games = np.split(temp, 19)
+
+    feat_games = [featurize(batch) for batch in batched_games]
+    featurized = np.vstack(feat_games)
+
+    os.remove('data/bitboard/' + game)
+    np.save('./data/features.npy', featurized)
+
